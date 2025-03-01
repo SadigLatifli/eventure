9d98dfd662e7ddbf74e9bb5eccc1427f0e895ad6
Sadiq Lətifli <sadiq.latifli@optima.az>
2025-03-02 03:36:24 +0400

    build errors fixed

--- src/views/test/pages/TestPage.tsx
+++
@@ -1,7 +0,0 @@
-import Test from '../components/Test';
-
-function TestPage () {
-  return <Test/>;
-};
-
-export default TestPage;
--- src/views/users/pages/UserListPage.tsx
+++
@@ -1,7 +0,0 @@
-import UserListnt from '../components/UserList';
-
-function UserListPage () {
-  return <UserListnt/>;
-};
-
-export default UserListPage;
